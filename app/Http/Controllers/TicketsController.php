fa06d54c6e3df495134f643a2edb185270f38aec
Phong <ttphong0812@gmail.com>
2018-07-16 11:40:21 +0700

    Finished Chapter 3

--- app/Http/Controllers/TicketsController.php
+++ app/Http/Controllers/TicketsController.php
@@ -8,6 +8,8 @@ use App\Http\Requests\TicketFormRequest;
 
 use App\Ticket;
 
+use Illuminate\Support\Facades\Mail;
+
 class TicketsController extends Controller
 {
     /**
@@ -17,7 +19,8 @@ class TicketsController extends Controller
      */
     public function index()
     {
-        //
+        $tickets = Ticket::all();
+        return view('tickets.index', compact('tickets'));
     }
 
     /**
@@ -47,6 +50,17 @@ class TicketsController extends Controller
 
         $ticket->save();
 
+        $data = array(
+            'ticket' => $slug,
+        );
+
+        Mail::send('emails.ticket', $data, function ($message) {
+            $message->from('ttphong0812@gmail.com', 'Learning Laravel');
+
+            $message->to('ttphong0812@gmail.com')->subject('There is a new ticket!');
+        });
+
+
         return redirect('/contact')->with('status', 'Your ticket has been created! Its unique ID is' .$slug);
     }
     
@@ -57,9 +71,11 @@ class TicketsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show($slug)
     {
-        //
+        $ticket = Ticket::whereSlug($slug)->firstOrFail();
+        $comments = $ticket->comments()->get();
+        return view('tickets.show',compact('ticket','comments'));
     }
 
     /**
@@ -68,9 +84,10 @@ class TicketsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit($slug)
     {
-        //
+        $ticket = Ticket::whereSlug($slug)->firstOrFail();
+        return view('tickets.edit',compact('ticket'));
     }
 
     /**
@@ -80,9 +97,19 @@ class TicketsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(TicketFormRequest $request, $slug)
     {
-        //
+        $ticket = Ticket::whereSlug($slug)->firstOrFail();
+        $ticket->title = $request->get('title');
+        $ticket->content = $request->get('content');
+        if($request->get('status') != null) {
+            $ticket->status = 0;
+        } else {
+            $ticket->status = 1;
+        }
+        $ticket->save();
+        return redirect(action('TicketsController@edit', $ticket->slug))->with('status', 'The ticket '.$slug.' has been updated!');
+
     }
 
     /**
@@ -91,8 +118,10 @@ class TicketsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy($slug)
     {
-        //
+        $ticket = Ticket::whereSlug($slug)->firstOrFail();
+        $ticket->delete();
+        return redirect('/tickets')->with('status', 'The ticket' .$slug. ' has been deleded!');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,45 @@
 |
 */
 
+//Get
+
 Route::get('/', 'PagesController@home');
 
 Route::get('/about', 'PagesController@about');
 
 Route::get('/contact', 'TicketsController@create');
 
+Route::get('/tickets','TicketsController@index');
+
+Route::get('/tickets/{slug?}', 'TicketsController@show');
+
+Route::get('/tickets/{slug?}/edit', 'TicketsController@edit');
+
+Route::get('sendemail', function () {
+
+    $data = array(
+        'name' => "Learning Laravel",
+    );
+
+    Mail::send('emails.welcome', $data, function ($message) {
+
+        $message->from('ttphong0812@gmail.com', 'Learning Laravel');
+
+        $message->to('ttphong0812@gmail.com')->subject('Learning Laravel test email');
+
+    });
+
+    return "Your email has been sent successfully";
+
+});
+
+
+//Post
+
 Route::post('/contact','TicketsController@store');
 
+Route::post('/tickets/{slug?}/edit', 'TicketsController@update');
+
+Route::post('/tickets/{slug?}/delete','TicketsController@destroy');
+
+Route::post('/comment', 'CommentsController@newComment');
